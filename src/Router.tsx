aa406858e3c826adcd05a2ebdabe0a6a8b469708
Nsholmes <metaproductions@gmail.com>
2023-11-05 11:09:11 -0600

    Added Judges entry

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,12 +2,14 @@ import { Route, createBrowserRouter, createRoutesFromElements } from "react-rout
 import App from "./App";
 import CreateEvent from "./Views/CreateEvent";
 import UploadEvent from "./Views/UploadEvent";
+import JudgeEntry from "./Views/JudgesEntry";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
       <Route path="createevent" element={<CreateEvent />} />
       <Route path="uploadEvent" element={<UploadEvent />} />
+      <Route path="judges" element={<JudgeEntry />} />
     </Route>
   )
 )
